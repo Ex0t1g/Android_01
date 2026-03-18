626ea7504528916095ebc4344519c7870d84b4bc
Ex0t1g <ex0t1g@mail.ru>
2026-03-18 21:23:05 +0300

    Init

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android.databinding.ActivityMainBinding
 import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private var onPauseTime: Long = 0
     private var onStopTime: Long = 0
     private var onDestroyTime: Long = 0
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
@@ -23,6 +26,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -32,59 +39,7 @@ class MainActivity : AppCompatActivity() {
         val endTime = System.currentTimeMillis()
         val executeTime = endTime - startTime
 
-        Log.d("[MainActivity]: ", "OnCreate was initialized... | in $executeTime ms");
     }
 
-
-    override fun onStart() {
-        super.onStart()
-        onStartTime = System.currentTimeMillis()
-
-        val diffOnCreateOnStart = onStartTime - onCreateTime
-        Log.d("[MainActivity]: ", "onStart was initialized...")
-        Log.d("[MainActivity]: ", "Time between onCreate and onStart: $diffOnCreateOnStart ms")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        onResumeTime = System.currentTimeMillis()
-
-        val diffOnCreateOnResume = onResumeTime - onCreateTime
-        val diffOnStartOnResume = onResumeTime - onStartTime
-
-        Log.d("[MainActivity]: ", "onResume was initialized...")
-        Log.d("[MainActivity]: ", "Time between onCreate and onResume: $diffOnCreateOnResume ms")
-        Log.d("[MainActivity]: ", "Time between onStart and onResume: $diffOnStartOnResume ms")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        onPauseTime = System.currentTimeMillis()
-
-        val diffOnResumeOnPause = onPauseTime - onResumeTime
-
-        Log.d("[MainActivity]: ", "onPause was initialized...")
-        Log.d("[MainActivity]: ", "Time between onResume and onPause: $diffOnResumeOnPause ms")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        onStopTime = System.currentTimeMillis()
-
-        val diffOnPauseOnStop = onStopTime - onPauseTime
-
-        Log.d("[MainActivity]: ", "onStop was initialized...")
-        Log.d("[MainActivity]: ", "Time between onPause and onStop: $diffOnPauseOnStop ms")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        onDestroyTime = System.currentTimeMillis()
-
-        val diffOnStopOnDestroy = onDestroyTime - onStopTime
-
-        Log.d("[MainActivity]: ", "onDestroy was initialized...")
-        Log.d("[MainActivity]: ", "Time between onStop and onDestroy: $diffOnStopOnDestroy ms")
-    }
 
 }
